b6ae288dc274b327d8783d3975bb2a5999ebd539
Valentín Sterba <valentin.sterba@gmail.com>
2024-11-27 15:41:39 -0300

    obstaculos y colisiones

--- game_main.py
+++ game_main.py
@@ -19,14 +19,27 @@ def draw_parallax():
         screen.blit(layer, (0, 0))
     
 clock = pygame.time.Clock()
+
+invisible_floor = SCREEN_HEIGHT - 30
+
 #COWBOY INFO - - - - - - - - - - - -  - - - - - - - - - - - - - -
 cowboy_x = 50
-cowboy_y = SCREEN_HEIGHT - 30
+cowboy_y = invisible_floor
 cowboy_width = 50
 cowboy_height = 50
 cowboy_speed_y = 0
 ground = True
 cowboy_Rect = pygame.Rect(cowboy_x,cowboy_y,cowboy_width,cowboy_height)
+
+# OBSTACLE INFO
+obstacle_width = 50
+obstacle_height = 100
+obstacle_x = SCREEN_WIDTH
+obstacle_y = invisible_floor - obstacle_height
+obstacle_speed = -10
+
+obstacle_Rect = pygame.Rect(obstacle_x, obstacle_y, obstacle_width, obstacle_height)
+
 
 
 
@@ -47,11 +60,21 @@ while (running):
     cowboy_Rect.y += cowboy_speed_y
     cowboy_speed_y += 1
     pygame.draw.rect(screen,(230,49,0),cowboy_Rect)
+
+    pygame.draw.rect(screen, (0, 0, 255), obstacle_Rect)
+    obstacle_Rect.x += obstacle_speed
+
+    if obstacle_Rect.x < 0 - obstacle_width:
+        obstacle_Rect.x = obstacle_x
         
-    if cowboy_Rect.bottom >= SCREEN_HEIGHT - 30:
-        cowboy_Rect.bottom = SCREEN_HEIGHT - 30
+    if cowboy_Rect.bottom >= invisible_floor:
+        cowboy_Rect.bottom = invisible_floor
         cowboy_speed_y = 0
         ground = True
+    
+    if cowboy_Rect.colliderect(obstacle_Rect):
+        print("Colisión")
+        running = False
         
     clock.tick(100)
     pygame.display.update()
