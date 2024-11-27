e631c4ffefebe39cba388e9887e535bb12d868ea
FranCoceres <franciscococeres040@gmail.com>
2024-11-27 20:18:50 -0300

    Commit Score

--- game_main.py
+++ game_main.py
@@ -1,5 +1,5 @@
 import pygame
-from vars_const import SCREEN_WIDTH, SCREEN_HEIGHT,touching_floor
+from vars_const import SCREEN_WIDTH, SCREEN_HEIGHT,touching_floor, BLACK
 
 pygame.init()
 
@@ -17,11 +17,24 @@ bg_layers = [pygame.transform.scale(layer, (SCREEN_WIDTH, SCREEN_HEIGHT)) for la
 def draw_parallax():
     for layer in bg_layers:
         screen.blit(layer, (0, 0))
-    
+
+def draw_text(text, font, color, surface, x, y):
+    '''Dibuja la superficie en la que el score se va a ubicar
+       Se le centra y da estilo con Font y color'''
+    text_obj = font.render(text, True, color)
+    text_rect = text_obj.get_rect(center=(x, y))
+    surface.blit(text_obj, text_rect) 
+
+def update_score(start_time):
+    ''' Calcular el tiempo transcurrido
+        Y controlar el limite de velocidad de aumento del score dividiendo // 100'''
+    elapsed_time = pygame.time.get_ticks() - start_time  
+    score = elapsed_time // 100  
+    return score
 clock = pygame.time.Clock()
 
 invisible_floor = SCREEN_HEIGHT - 30
-
+font = pygame.font.Font(None, 50)
 #COWBOY INFO - - - - - - - - - - - -  - - - - - - - - - - - - - -
 cowboy_x = 50
 cowboy_y = invisible_floor
@@ -44,7 +57,7 @@ obstacle_Rect = pygame.Rect(obstacle_x, obstacle_y, obstacle_width, obstacle_hei
 
 
 
-
+start_time = pygame.time.get_ticks()
 running = True
 while (running):
     draw_parallax()
@@ -53,7 +66,10 @@ while (running):
         if event.type == pygame.QUIT:
             running = False
     keys = pygame.key.get_pressed()
-
+    
+    score = update_score(start_time)
+    draw_text(f"Score: {score}", font, BLACK, screen, SCREEN_WIDTH // 2, 50)
+    
     if keys[pygame.K_SPACE] and ground:
         cowboy_speed_y = -20
         ground = False
@@ -75,7 +91,7 @@ while (running):
     if cowboy_Rect.colliderect(obstacle_Rect):
         print("Colisión")
         running = False
-        
+    
     clock.tick(100)
     pygame.display.update()
 pygame.quit()
